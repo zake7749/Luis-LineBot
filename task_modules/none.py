5620237469752ccac54cb94706a1ad05d987921c
zake7749 <zake7749@gmail.com>
2017-06-08 22:43:35 +0800

    Add a handler for None intent.

---
+++ task_modules/none.py
@@ -0,0 +1,9 @@
+from .task import TaskHandler
+
+class NoneHandler(TaskHandler):
+
+    def __init__(self):
+        super(NoneHandler, self).__init__()
+
+    def get_response(self, entity='None'):
+        return "這部分仍在施工中"
--- task_modules/task_switch.py
+++ task_modules/task_switch.py
@@ -1,11 +1,13 @@
 from .restaurant import FoodHandler
 from .activity import ActivityHandler
 from .weather import WeatherHandler
+from .none import NoneHandler
 
 class TaskSwitcher(object):
 
     def __init__(self):
 
+        self.none_handler = NoneHandler()
         self.food_handler = FoodHandler()
         self.weather_handler = WeatherHandler()
         self.activity_handler = ActivityHandler()
@@ -35,10 +37,13 @@ class TaskSwitcher(object):
             return self.activity_handler
 
         if intent == "Greeting":
-            return None
+            return self.none_handler
 
         if intent == "Weather":
             return self.weather_handler
 
         if intent == "Speech":
-            return None
+            return self.none_handler
+
+        if intent == "None":
+            return self.none_handler
